3aad708af55e25228876cb98c193d17a4de19650
Arcedo <arcedo.marc@gmail.com>
2024-10-31 18:11:29 +0100

    Fix job search anchor

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,6 @@ export const GITHUB_URL = 'https://github.com/arcedo/';
 export const RESUME_URL = '/cv_marc_arcedo.pdf';
 export const EMAIL_ADDRESS = 'arcedo.marc@gmail.com';
 export const SECTIONS = ['home', 'experience', 'projects', 'contact'];
-export const START_DATE_SEARCH_JOB = new Date('2024-09-04');
+export const START_DATE_SEARCH_JOB = new Date('2024-09-12');
 export const GOOGLE_TAG = 'G-...';
 export const ENDPOINT = 'https://api.arcedo.dev/';
