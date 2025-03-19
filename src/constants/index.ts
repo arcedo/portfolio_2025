3be5cc1034d9dcebeec97e53169c97761674f496
Marc Arcedo <arcedo.marc@gmail.com>
2025-03-19 16:22:30 +0100

    fixes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const LINKEDIN_URL = "https://www.linkedin.com/in/m-arcedo/";
 export const GITHUB_URL = "https://github.com/arcedo/";
-export const RESUME_URL = "/cv_marc_arcedo.pdf";
+export const RESUME_URL = "/cv_marc_arcedo_en.pdf";
 export const EMAIL_ADDRESS = "arcedo.marc@gmail.com";
 export const SECTIONS = ["home", "experience", "projects", "contact"];
 export const START_DATE_SEARCH_JOB = "";
